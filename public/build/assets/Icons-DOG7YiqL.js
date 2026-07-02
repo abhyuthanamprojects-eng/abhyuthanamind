d7d2db8fde4a434aaf3340a6d0b6a2fc5c056a6b
Amit Sinha <amitsinhadeveloper@gmail.com>
2026-07-02 11:26:05 +0530

    Updated the imagess

---
+++ public/build/assets/Icons-DOG7YiqL.js
@@ -0,0 +1,31 @@
+import{j as e}from"./index-DXibOfsm.js";import{A as s,B as t,U as r,a as i}from"./AdminLayout-mS9FiKED.js";import{A as l}from"./AdminHeader-B5USm9zi.js";import{H as m}from"./app-2UYAZFm9.js";import{c as a}from"./createLucideIcon-DhKg2NRa.js";import{M as d}from"./mail-P3ZRBQZQ.js";import{C as y}from"./calendar-DIpSaTqx.js";import{_ as x,Y as h,a3 as p,U as u,a5 as I,N as k,a8 as g}from"./utils-5qjUTMFs.js";import{H as f}from"./heart-Ba1aloyz.js";import{E as j}from"./earth-DTwe_3x9.js";import"./plus-f8uviBb1.js";/* empty css            *//**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"r6nss1"}]],_=a("house",v);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["rect",{width:"7",height:"7",x:"3",y:"3",rx:"1",key:"1g98yp"}],["rect",{width:"7",height:"7",x:"14",y:"3",rx:"1",key:"6d4xhi"}],["rect",{width:"7",height:"7",x:"14",y:"14",rx:"1",key:"nxv5o0"}],["rect",{width:"7",height:"7",x:"3",y:"14",rx:"1",key:"1bb6yr"}]],H=a("layout-grid",M);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=[["path",{d:"m10 17 5-5-5-5",key:"1bsop3"}],["path",{d:"M15 12H3",key:"6jk70r"}],["path",{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4",key:"u53s6r"}]],C=a("log-in",N);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=[["path",{d:"M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915",key:"1i5ecw"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]],L=a("settings",w);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const S=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M8 14s1.5 2 4 2 4-2 4-2",key:"1y1vjs"}],["line",{x1:"9",x2:"9.01",y1:"9",y2:"9",key:"yxxnd0"}],["line",{x1:"15",x2:"15.01",y1:"9",y2:"9",key:"1p4y9e"}]],o=a("smile",S);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["line",{x1:"19",x2:"19",y1:"8",y2:"14",key:"1bvyxn"}],["line",{x1:"22",x2:"16",y1:"11",y2:"11",key:"1shjgl"}]],A=a("user-plus",b),U=[{name:"Smile",Icon:o},{name:"Home",Icon:_},{name:"Settings",Icon:L},{name:"Mail",Icon:d},{name:"Bell",Icon:t},{name:"User",Icon:r},{name:"Calendar",Icon:y},{name:"Search",Icon:x},{name:"Star",Icon:h},{name:"Heart",Icon:f},{name:"Globe2",Icon:j},{name:"FileText",Icon:p},{name:"LayoutGrid",Icon:H},{name:"CheckCircle2",Icon:u},{name:"XCircle",Icon:I},{name:"Menu",Icon:k},{name:"LogIn",Icon:C},{name:"LogOut",Icon:i},{name:"UserPlus",Icon:A},{name:"HelpCircle",Icon:g}];function R(){return e.jsxs(s,{children:[e.jsx(m,{title:"Icons"}),e.jsx(l,{title:"Icons",subtitle:"lucide-react icon set used throughout the admin panel.",icon:e.jsx(o,{size:20})}),e.jsx("div",{className:"card",children:e.jsx("div",{className:"card-body",children:e.jsx("div",{className:"row g-3",children:U.map(({name:n,Icon:c})=>e.jsxs("div",{className:"col-6 col-md-3 col-lg-2 d-flex flex-column align-items-center text-center gap-2 py-3",children:[e.jsx(c,{size:24}),e.jsx("span",{className:"fs-2 text-secondary",children:n})]},n))})})})]})}export{R as default};
